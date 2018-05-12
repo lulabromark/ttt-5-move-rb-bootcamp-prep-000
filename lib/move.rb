60e9fa98220003bf695c2687af5df73ae7144885
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-12 03:25:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, position, index = "X")
-  board[position] = index
+def move(board, position, value = "X")
+  board[position] = value
 end
